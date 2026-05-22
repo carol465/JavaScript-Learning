f45505be238888a5ed738c95e7be63b1c5c94487
Carolina Pereira <a88911967@gmail.com>
2026-05-22 15:25:22 +0100

    strip property

--- 2StripProperty.js
+++ 2StripProperty.js
@@ -6,13 +6,15 @@ Retorna um novo objeto (ou o mesmo, conforme variação) sem essa chave.
 Se a propriedade não existir, retorna o objeto inalterado. 
 */
 
-const user ={
-    username = "Carol",
-    age = 28,
-    password = "465"
+const user ={ //objeto
+    username: "Carol", //propriedade
+    age: 28, //propriedade
+    password: "465" //propriedade
 }
 
-function stripProperty(password){
-    delete user.password;
-    console.log(user);
+function stripProperty(obj, property) { // usar uma função generica
+    delete obj[property];               // depois é que defino o que quero apagar
+    return obj;
 }
+
+console.log(stripProperty(user, "password")); // obj= user, property= password
